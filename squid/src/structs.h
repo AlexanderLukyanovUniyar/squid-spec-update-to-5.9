83a9b222f839c76f0ed5747baf44a7a8598dbcc8
Grigory Batalov <bga@altlinux.org>
2007-09-27 23:15:49 +0400

    2.6.STABLE14

--- squid/src/structs.h
+++ squid/src/structs.h
@@ -1,6 +1,6 @@
 
 /*
- * $Id: structs.h,v 1.507.2.5 2007/04/26 23:11:51 hno Exp $
+ * $Id: structs.h,v 1.507.2.7 2007/07/15 09:52:18 hno Exp $
  *
  *
  * SQUID Web Proxy Cache          http://www.squid-cache.org/
@@ -1755,6 +1755,7 @@ struct _SwapDir {
     int max_size;
     char *path;
     int index;			/* This entry's index into the swapDirs array */
+    squid_off_t min_objsize;
     squid_off_t max_objsize;
     RemovalPolicy *repl;
     int removals;
@@ -1814,6 +1815,7 @@ struct _request_flags {
     unsigned int cachable:1;
     unsigned int hierarchical:1;
     unsigned int loopdetect:1;
+    unsigned int loopdetect_twice:1;
     unsigned int proxy_keepalive:1;
     unsigned int proxying:1;	/* this should be killed, also in httpstateflags */
     unsigned int refresh:1;

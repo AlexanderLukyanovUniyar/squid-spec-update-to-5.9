3b3b8c48b2a334ceb1e32cc2d16470c7b8056523
Vitaly Kuznetsov <vitty@altlinux.ru>
2011-07-01 13:28:02 +0000

    3.1.13 sources

--- squid/src/forward.cc
+++ squid/src/forward.cc
@@ -48,6 +48,10 @@
 #include "icmp/net_db.h"
 #include "ip/IpIntercept.h"
 #include "ip/tools.h"
+#if USE_SSL
+#include "ssl_support.h"
+#include "ssl/ErrorDetail.h"
+#endif
 
 static PSC fwdStartCompleteWrapper;
 static PF fwdServerClosedWrapper;
@@ -605,6 +609,14 @@ FwdState::negotiateSSL(int fd)
             anErr->xerrno = EACCES;
 #endif
 
+            Ssl::ErrorDetail *errFromFailure = (Ssl::ErrorDetail *)SSL_get_ex_data(ssl, ssl_ex_index_ssl_error_detail);
+            if (errFromFailure != NULL) {
+                // The errFromFailure is attached to the ssl object
+                // and will be released when ssl object destroyed.
+                // Copy errFromFailure to a new Ssl::ErrorDetail object
+                anErr->detail = new Ssl::ErrorDetail(*errFromFailure);
+            }
+
             fail(anErr);
 
             if (fs->_peer) {
@@ -820,6 +832,13 @@ FwdState::connectStart()
     if (ftimeout < ctimeout)
         ctimeout = ftimeout;
 
+    if (fs->_peer && request->flags.sslBumped == true) {
+        debugs(50, 4, "fwdConnectStart: Ssl bumped connections through parrent proxy are not allowed");
+        ErrorState *anErr = errorCon(ERR_CANNOT_FORWARD, HTTP_SERVICE_UNAVAILABLE, request);
+        fail(anErr);
+        self = NULL; // refcounted
+        return;
+    }
 
     request->flags.pinned = 0;
     if (fs->code == PINNED) {
@@ -871,6 +890,9 @@ FwdState::connectStart()
         updateHierarchyInfo();
 
         comm_add_close_handler(fd, fwdServerClosedWrapper, this);
+
+        if (comm_local_port(fd))
+            request->hier.peer_local_addr = fd_table[fd].local_addr;
 
         dispatch();
 
@@ -930,6 +952,9 @@ FwdState::connectStart()
 
     if (!fs->_peer)
         origin_tries++;
+
+    if (comm_local_port(fd))
+        request->hier.peer_local_addr = fd_table[fd].local_addr;
 
     /*
      * stats.conn_open is used to account for the number of
